3a74669a59dc6d3c1ff30adfb2129d31f08b0ecb
Johnny Hildebrand <JHVJD1@gmail.com>
2022-07-31 19:12:06 -0500

    Fix Error on student table Add OnClick evnets and JS

--- Course_Registration/Controllers/HomeController.cs
+++ Course_Registration/Controllers/HomeController.cs
@@ -23,20 +23,20 @@ namespace Course_Registration.Controllers
 							 Instructor instructs = new Instructor();
 							 List<Instructor> Inst = new List<Instructor>()
 	{
-	new Instructor{ InstructorId = 1, 
-			 FirstName = "Bernard", 
-			 LastName = "Shins", 
-			 EmailAddress = "BernardShins@Gmail.com", 
+	new Instructor{ InstructorId = 1,
+			 FirstName = "Bernard",
+			 LastName = "Shins",
+			 EmailAddress = "BernardShins@Gmail.com",
 			 Course = "Data Entry" },
-	new Instructor{ InstructorId = 2, 
-			 FirstName= "Wilma", 
-			 LastName = "Gunner", 
-			 EmailAddress = "WilmaGunner@Gmail.com", 
+	new Instructor{ InstructorId = 2,
+			 FirstName= "Wilma",
+			 LastName = "Gunner",
+			 EmailAddress = "WilmaGunner@Gmail.com",
 			 Course = "Word Processing" },
-	new Instructor{ InstructorId = 3, 
-			 FirstName= "Monte", 
-			 LastName= "Luxenburg", 
-			 EmailAddress ="MonteLuxenberg@Gmail.com", 
+	new Instructor{ InstructorId = 3,
+			 FirstName= "Monte",
+			 LastName= "Luxenburg",
+			 EmailAddress ="MonteLuxenberg@Gmail.com",
 			 Course = "Typing" }
 	};
 							 instructs.Instructors = Inst;
@@ -50,19 +50,19 @@ namespace Course_Registration.Controllers
 							 List<Student> Stu = new List<Student>()
 	{
 	new Student{ StudentId = 1,
-			 FirstName = "Thomas", 
-			 LastName = "Train", 
-			 EmailAddress = "ThomasTrain@Gmail.com", 
+			 FirstName = "Thomas",
+			 LastName = "Train",
+			 EmailAddress = "ThomasTrain@Gmail.com",
 			 PhoneNumber = "1-204-555-1245" },
-	new Student{ StudentId= 2, 
-			 FirstName= "Julia", 
-			 LastName = "Hubert", 
-			 EmailAddress = "JuliaHubert@Hotmail.com", 
+	new Student{ StudentId= 2,
+			 FirstName= "Julia",
+			 LastName = "Hubert",
+			 EmailAddress = "JuliaHubert@Hotmail.com",
 			 PhoneNumber = "1-555-159-9988" },
-	new Student{ StudentId = 3, 
-			 FirstName= "Mark", 
-			 LastName= "Walberger", 
-			 EmailAddress ="MarkWalberger@Gmail.com", 
+	new Student{ StudentId = 3,
+			 FirstName= "Mark",
+			 LastName= "Walberger",
+			 EmailAddress ="MarkWalberger@Gmail.com",
 			 PhoneNumber = "1-985-666-8912" }
 	};
 							 students.Students = Stu;
@@ -96,16 +96,50 @@ namespace Course_Registration.Controllers
 								 }
 						}
 			 },
-	new Course{ 
-			 CourseId= 2, 
-			 CourseNumber = 31, 
-			 CourseName = "Word Processing", 
-			 Description = "This training provide an knowledge in use of office automation, internet and internet tools." },
-	new Course{ 
-			 CourseId = 3, 
-			 CourseNumber = 11, 
-			 CourseName= "Typing", 
-			 Description = "Typing is the process of writing or inputting text by pressing keys on a typewriter, computer keyboard, cell phone, or calculator." },
+	new Course{
+			 CourseId= 2,
+			 CourseNumber = 31,
+			 CourseName = "Word Processing",
+			 Description = "This training provide an knowledge in use of office automation, internet and internet tools.",
+
+			 Students = new List<Student>
+						{
+						new Student
+								 {
+								 StudentId = 12
+								 },
+						new Student
+								 {
+								 StudentId = 22
+								 },
+						new Student
+								 {
+								 StudentId = 34
+								 }
+						}
+
+			 },
+	new Course{
+			 CourseId = 3,
+			 CourseNumber = 11,
+			 CourseName= "Typing",
+			 Description = "Typing is the process of writing or inputting text by pressing keys on a typewriter, computer keyboard, cell phone, or calculator.",
+			 Students = new List<Student>
+						{
+						new Student
+								 {
+								 StudentId = 23
+								 },
+						new Student
+								 {
+								 StudentId = 20
+								 },
+						new Student
+								 {
+								 StudentId = 33
+								 }
+						}
+			 },
 	};
 							 courses.Courses = Cor;
 							 return View(courses);
--- Course_Registration/wwwroot/js/site.js
+++ Course_Registration/wwwroot/js/site.js
@@ -1,15 +1,15 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-const { selectors } = require("sizzle");
+
 
 // Write your JavaScript code.
 
 function showModal(CourseId) {
-		 let selector = "course-student-modal-" + CourseId;
-		 $(selectors).show();
+		 let selector = ".course-student-modal-" + CourseId;
+		 $(selector).show();
 }
 function hideModal(CourseId) {
-		 let selector = "course-student-modal-" + CourseId;
-		 $(selectors).hide();
+		 let selector = ".course-student-modal-" + CourseId;
+		 $(selector).hide();
 }
