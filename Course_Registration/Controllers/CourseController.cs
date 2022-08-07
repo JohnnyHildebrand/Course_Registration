75eff3c25db371e3bdf783ceadbfa85079015c83
Johnny Hildebrand <JHVJD1@gmail.com>
2022-08-07 14:05:02 -0500

    Add Course Controller Updated  controller in layout.chtml

---
+++ Course_Registration/Controllers/CourseController.cs
@@ -0,0 +1,56 @@
+﻿using Course_Registration.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course_Registration.Controllers
+		{
+		public class CourseController : Controller
+				{
+				/*private readonly CourseContext _context;
+
+				public CourseController()
+						{
+						_context = new CourseContext();
+						}
+
+				public IActionResult Courses()
+						{
+						var courses = _context.Courses.Include(c => c.Students).ToList();
+						return View(courses);
+						}*/
+
+				public IActionResult Course()
+						{
+						Course courses = new Course();
+						List<Course> Cor = new List<Course>()
+
+											{
+		new Course{
+					Id = 1,
+					CourseNumber  = 23,
+					CourseName = "C Sharp",
+					Description= "This course is an introduction to computer programming for Windows. Emphasis will be on the fundamentals of structured design, development, testing, implementation, and documentation, including language syntax, data and file structures, input/output devices, files, and databases",
+					Students = new List<Student>
+							{
+							new Student
+										{
+									Id = 123
+										},
+							new Student
+										{
+									Id = 221
+										},
+							new Student
+										{
+										Id = 334
+										}
+							}
+					},
+
+
+		};
+						courses.Courses = Cor;
+						return View(courses);
+						}
+				}
+		}
+
---
+++ Course_Registration/DTO/Mapper.cs
@@ -0,0 +1,75 @@
+﻿using Course_Registration.Models;
+
+namespace Course_Registration.DTO
+		{
+		public class Mapper
+				{
+				public StudentDTO Map(Student student)
+						{
+						return new StudentDTO
+								{
+								Id = student.Id,
+								FirstName = student.FirstName,
+								LastName = student.LastName,
+								EmailAddress = student.EmailAddress,
+								PhoneNumber = student.PhoneNumber,
+								};
+						}
+				public Student Map(StudentDTO studentDTO)
+						{
+						return new Student
+								{
+								Id = studentDTO.Id,
+								FirstName = studentDTO.FirstName,
+								LastName = studentDTO.LastName,
+								EmailAddress = studentDTO.EmailAddress,
+								PhoneNumber = studentDTO.PhoneNumber,
+								};
+						}
+				public CourseDTO Map(Course course)
+						{
+						return new CourseDTO
+								{
+								Id = course.Id,
+								CourseNumber = course.CourseNumber,
+								CourseName = course.CourseName,
+								Description = course.Description,
+								};
+						}
+				public Course Map(CourseDTO courseDTO)
+						{
+						return new Course
+								{
+								Id=courseDTO.Id,
+								CourseNumber=courseDTO.CourseNumber,
+								CourseName=courseDTO.CourseName,
+								Description=courseDTO.Description,
+								};
+						}
+				public InstructorDTO Map(Instructor instructor)
+						{
+						return new InstructorDTO
+								{
+								Id = instructor.Id,
+								FirstName = instructor.FirstName,
+								LastName = instructor.LastName,
+								EmailAddress = instructor.EmailAddress,
+								Course = instructor.Course,
+								};
+						}
+				public Instructor Map(InstructorDTO instructorDTO)
+						{
+						return new Instructor
+								{
+								Id = instructorDTO.Id,
+								FirstName = instructorDTO.FirstName,
+								LastName = instructorDTO.LastName,
+								EmailAddress = instructorDTO.EmailAddress,
+								Course=instructorDTO.Course,
+								};
+						}
+				}
+		}
+
+
+
